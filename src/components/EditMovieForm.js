8f484be09719243bf4a4dfc1c678d32f483e6408
bugsbrog <bugsbrog@gmail.com>
2021-12-15 14:37:47 -0700

    Made inputs in EditMovieForm.js look nicer

--- src/components/EditMovieForm.js
+++ src/components/EditMovieForm.js
@@ -56,29 +56,60 @@ const EditMovieForm = (props) => {
 				<div className="modal-body">					
 					<div className="form-group">
 						<label>Title</label>
-						<input value={title} onChange={handleChange} name="title" type="text" className="form-control"/>
+						<input
+							value={title}
+							onChange={handleChange}
+							name="title"
+							type="text"
+							className="form-control"/>
 					</div>
 					<div className="form-group">
 						<label>Director</label>
-						<input value={director} onChange={handleChange} name="director" type="text" className="form-control"/>
+						<input
+							value={director}
+							onChange={handleChange}
+							name="director"
+							type="text"
+							className="form-control"/>
 					</div>
 					<div className="form-group">
 						<label>Genre</label>
-						<input value={genre} onChange={handleChange} name="genre" type="text" className="form-control"/>
+						<input
+							value={genre}
+							onChange={handleChange}
+							name="genre"
+							type="text"
+							className="form-control"/>
 					</div>
 					<div className="form-group">
 						<label>Metascore</label>
-						<input value={metascore} onChange={handleChange} name="metascore" type="number" className="form-control"/>
+						<input
+							value={metascore}
+							onChange={handleChange}
+							name="metascore"
+							type="number"
+							className="form-control"/>
 					</div>		
 					<div className="form-group">
 						<label>Description</label>
-						<textarea value={description} onChange={handleChange} name="description" className="form-control"></textarea>
+						<textarea
+							value={description}
+							onChange={handleChange}
+							name="description"
+							className="form-control"></textarea>
 					</div>
 									
 				</div>
 				<div className="modal-footer">			    
-					<input type="submit" className="btn btn-info" value="Save"/>
-					<Link to={`/movies/1`}><input type="button" className="btn btn-default" value="Cancel"/></Link>
+					<input
+						type="submit"
+						className="btn btn-info"
+						value="Save"/>
+					<Link to={`/movies/1`}>
+						<input
+							type="button"
+							className="btn btn-default"
+							value="Cancel"/></Link>
 				</div>
 			</form>
 		</div>
